ae300cf5ca01b33b7ef8ddeb448eb5050751ae37
Koushik Sankarasetty <sankarasetty.koushik@click2cloud.net>
2022-05-31 18:15:10 +0530

    removed unnecessary code

--- app/main.py
+++ app/main.py
@@ -29,21 +29,6 @@ while True:
         print("Error:",  error)
         time.sleep(1)
 
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {
-    "title": "title of post 2", "content": "content of post 2", "id": 2}]
-
-
-def find_post(id):
-    for it in my_posts:
-        if it['id'] == id:
-            return it
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
 
 @app.get("/")
 def read_root():
