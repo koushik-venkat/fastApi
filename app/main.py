da086595e06c5dc50e4b0f180eaae3a78b31308b
Koushik Sankarasetty <sankarasetty.koushik@click2cloud.net>
2022-06-03 17:30:59 +0530

    new commit

--- app/main.py
+++ app/main.py
@@ -7,8 +7,19 @@ from random import randrange
 import psycopg2
 import time
 from psycopg2.extras import RealDictCursor
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:4200"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Post(BaseModel):
